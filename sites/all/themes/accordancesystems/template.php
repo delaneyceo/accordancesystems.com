e017f45d3754c8d900fce009d79acd1994a650c5
Adam Delaney <adam-delaney@uiowa.edu>
2015-02-10 18:56:22 -0600

    updated theme assets

--- sites/all/themes/accordancesystems/template.php
+++ sites/all/themes/accordancesystems/template.php
@@ -5,3 +5,40 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * accordancesystems theme.
  */
+
+/**
+ * Implements theme_breadcrumb().
+ */
+function accordancesystems_breadcrumb(&$variables) {
+  $output = '';
+  if (!empty($variables['breadcrumb'])) {
+    $output = '<div id="breadcrumb" class="breadcrumb-container clearfix"><h2 class="element-invisible">You are here</h2><ul class="breadcrumb">';
+    $switch = array('odd' => 'even', 'even' => 'odd');
+    $zebra = 'even';
+    $last = count($variables['breadcrumb']) - 1;
+    $seperator = '<span class="breadcrumb-seperator">/</span>';
+    foreach ($variables['breadcrumb'] as $key => $item) {
+      $zebra = $switch[$zebra];
+      $attributes['class'] = array('depth-' . ($key + 1), $zebra);
+      if ($key == 0) {
+        $attributes['class'][] = 'first';
+      }
+      if ($key == $last) {
+        $attributes['class'][] = 'last';
+        $output .= '<li' . drupal_attributes($attributes) . '>' . $item . '</li>' . '';
+      }
+      else
+        $output .= '<li' . drupal_attributes($attributes) . '>' . $item . '</li>' . $seperator;
+      }
+    $output .= '</ul></div>';
+    return $output;
+  }
+}
+
+function accordancesystems_css_alter(&$css) {
+  // Dynamically remove css for temporary splash page.
+  if (drupal_is_front_page()) {
+    // Remove defaults.css file.
+    unset($css[drupal_get_path('theme', 'accordancesystems') . '/css/accordancesystems.styles.css']);
+  }
+}
